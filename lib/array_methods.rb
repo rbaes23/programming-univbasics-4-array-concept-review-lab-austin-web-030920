79afb1199735f38becedbb2a9b5926ce532591e6
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-20 21:27:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,10 +18,10 @@ end
 #  return array.max
 #end
 
-def find_max_value(array)
-  x = 0
-  array.length.times { |index| x = array[index] if array[index] > x } 
-  x
+#def find_max_value(array)
+#  x = 0
+#  array.length.times { |index| x = array[index] if array[index] > x } 
+#  x
 end
 
 def find_min_value(array)
